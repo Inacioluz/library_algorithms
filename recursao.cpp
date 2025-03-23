e16456c94bff15cdc3659b0db79c5db71ba42ecf
Inacioluz <joseinaciolds@gmail.com>
2025-03-23 18:11:36 -0300

    recursão

--- recursao.cpp
+++ recursao.cpp
@@ -1,14 +1,27 @@
 #include <iostream> // Inclui a biblioteca de entrada e saída padrão
 using namespace std; // Usa o namespace padrão para evitar escrever std:: antes de cada comando
 
-// Função recursiva que calcula o fatorial de um número inteiro
-int fatorial(int n) {
-    if(n == 0) return 1; // Caso base: o fatorial de 0 é 1
-    return n * fatorial(n - 1); // Chamada recursiva: n * fatorial de (n-1)
-}
+// // Função recursiva que calcula o fatorial de um número inteiro
+// int fatorial(int n) {
+//     if(n == 0) return 1; // Caso base: o fatorial de 0 é 1
+//     return n * fatorial(n - 1); // Chamada recursiva: n * fatorial de (n-1)
+// }
+
+void imprime(int contador ) {
+    if(contador == 0)
+        return;
+        else {
+            cout << "amoeba" << endl;
+            imprime(contador - 1);
+        }
+    }
 
 int main() {
-    // Imprime o resultado do fatorial de 5
-    cout << "Fatorial de 5: " << fatorial(5) << endl;
+    // // Imprime o resultado do fatorial de 5
+    // cout << "Fatorial de 5: " << fatorial(5) << endl;
+    
+    imprime(10);
+    
+       
     return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
